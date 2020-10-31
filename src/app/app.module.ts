a12c69f07fe57c79ff62b3f6e39a3288d518cde9
Kotsanai Chikosi <krcchiko@gmail.com>
2020-10-31 04:00:32 -0400

    add api calls to backend server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,17 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
+/* student components */
 import { AddStudentComponent } from './components/add-student/add-student.component';
 import { EditStudentComponent } from './components/edit-student/edit-student.component';
 import { StudentsListComponent } from './components/students-list/students-list.component';
+
+/* angular http service */
+import { HttpClientModule } from '@angular/common/http';
+/* angular crud service */
+import { ApiService } from './shared/api.service';
+/* angular reactive forms */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,8 +31,11 @@ import { StudentsListComponent } from './components/students-list/students-list.
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
